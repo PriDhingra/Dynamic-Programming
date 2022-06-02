2d72d3f82da71e88cfc95b7ebfa3341acf8b1368
Prince Dhingra <princedhingra25@gmail.com>
2022-06-01 22:14:08 -0400

    Update climbingStairs.cpp

--- climbingStairs.cpp
+++ climbingStairs.cpp
@@ -1,3 +1,4 @@
+//Memoization
 class Solution {
 public:
     int solve(int n, vector<int> &dp) {
@@ -17,5 +18,24 @@ public:
         solve(n, dp);
         
         return dp[n];
+    }
+};
+
+
+//BottomUp
+class Solution {
+public:
+    int climbStairs(int n) {
+        int index = 0;
+        vector<int> dp(n + 1, 0);
+        dp[n] = 1;
+        
+        for(int i = n - 1;i >= 0;i--) {
+            dp[i] += dp[i + 1];
+            if(i + 2 <= n)
+                dp[i] += dp[i + 2];
+        }
+        
+        return dp[0];
     }
 };
