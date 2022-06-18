0e1e1bc02f4408a73be162dd8ac5dbc854eb3071
Prince Dhingra <princedhingra25@gmail.com>
2022-06-18 14:16:01 -0400

    Update 01Knapsack.cpp

--- 01Knapsack.cpp
+++ 01Knapsack.cpp
@@ -24,3 +24,30 @@ int knapsack(vector<int> weight, vector<int> value, int n, int maxWeight)
     vector<vector<int>> dp(n, vector<int>(maxWeight + 1, -1));
     return f(n - 1, maxWeight, weight, value, dp);
 }
+
+
+
+//Tabulation
+
+int knapsack(vector<int> wt, vector<int> val, int n, int maxWeight) 
+{
+	// Write your code here
+    vector<vector<int>> dp(n, vector<int>(maxWeight + 1, 0));
+    
+    for(int W = wt[0];W <= maxWeight;W++) 
+        dp[0][W] = val[0];
+    
+    for(int ind = 1;ind < n;ind++) {
+        for(int W = 0;W <= maxWeight;W++) {
+            int notTake = 0 + dp[ind - 1][W];
+            int take = INT_MIN;
+
+            if(wt[ind] <= W)
+                take = val[ind] + dp[ind - 1][W - wt[ind]];
+
+            dp[ind][W] = max(take, notTake);
+        }
+    }
+    
+    return dp[n - 1][maxWeight];
+}
