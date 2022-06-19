dfbf06c2c63b1987d42d7ef5286574fff2570905
Prince Dhingra <princedhingra25@gmail.com>
2022-06-19 11:23:58 -0400

    Update unboundedKnapsack.cpp

--- unboundedKnapsack.cpp
+++ unboundedKnapsack.cpp
@@ -20,3 +20,30 @@ int unboundedKnapsack(int n, int w, vector<int> &profit, vector<int> &weight)
     vector<vector<int>> dp(n, vector<int>(w + 1, -1));
     return f(n - 1, w, profit, weight, dp);
 }
+
+
+//Tabulation
+
+int unboundedKnapsack(int n, int w, vector<int> &profit, vector<int> &weight)
+{
+    // Write Your Code Here.
+    vector<vector<int>> dp(n, vector<int>(w + 1, 0));
+    
+    for(int W = 0;W <= w;W++) {
+        dp[0][W] = W / weight[0] * profit[0];
+    }
+    
+    for(int ind = 1;ind < n;ind++) {
+        for(int W = 0;W <= w;W++) {
+            int notTake = 0 + dp[ind - 1][W];
+            int take = -1e8;
+            if(weight[ind] <= W)
+                take = profit[ind] + dp[ind][W - weight[ind]];
+
+            dp[ind][W] = max(take, notTake);
+        }
+    }
+    return dp[n - 1][w];
+//     return f(n - 1, w, profit, weight, dp);
+}
+
