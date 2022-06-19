db902682fa4df8636e7022526d3630d44f215cd8
Prince Dhingra <princedhingra25@gmail.com>
2022-06-19 14:02:11 -0400

    Update longestCommonSubsequence.cpp

--- longestCommonSubsequence.cpp
+++ longestCommonSubsequence.cpp
@@ -1,21 +1,22 @@
-class Solution {
-public:
-    int longestCommonSubsequence(string text1, string text2) {
-        int size1 = text1.size();
-        int size2 = text2.size();
-        int dp[size1 + 1][size2 + 1];
-        
-        memset(dp, 0, sizeof(dp));
-        
-        for(int i = size1 - 1;i >= 0;i--) {
-            for(int j = size2 - 1;j >= 0;j--) {
-                if(text1[i] == text2[j]) 
-                    dp[i][j] = 1 + dp[i + 1][j + 1];
-                else 
-                    dp[i][j] = max(dp[i + 1][j], dp[i][j + 1]);
-            }
-        }
-        
-        return dp[0][0];
-    }
-};
+#include<vector>
+int f(int i, int j, string &s, string &t, vector<vector<int>> &dp) {
+    if(i < 0 || j < 0)
+        return 0;
+    
+    if(dp[i][j] != -1)
+        return dp[i][j];
+    
+    if(s[i] == t[j])
+        return 1 + f(i - 1, j - 1, s, t, dp);
+    
+    return dp[i][j] = max(f(i - 1, j, s, t, dp), f(i, j - 1, s, t, dp));
+}
+
+int lcs(string s, string t)
+{
+	//Write your code here
+    int n = s.size();
+    int m = t. size();
+    vector<vector<int>> dp(n, vector<int>(m, -1));
+    return f(n - 1, m - 1, s, t, dp);
+}
