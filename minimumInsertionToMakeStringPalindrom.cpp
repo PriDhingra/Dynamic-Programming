87a5b5aa5fc7e4fdfdbb04b21762381a26fd3750
Prince Dhingra <princedhingra25@gmail.com>
2022-06-20 20:50:55 -0400

    Create minimumInsertionToMakeStringPalindrom.cpp

---
+++ minimumInsertionToMakeStringPalindrom.cpp
@@ -0,0 +1,29 @@
+#include<vector>
+int lcs(string s, string t, int n) {
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+        
+        for(int i = n - 1;i >= 0;i--) {
+            for(int j = n - 1;j >= 0;j--) {
+                if(s[i] == t[j])
+                    dp[i][j] = 1 + dp[i + 1][j + 1];
+                else
+                    dp[i][j] = max(dp[i + 1][j], dp[i][j + 1]);
+            }
+        }
+        
+        return dp[0][0];
+    }
+    
+int longestPalindromeSubseq(string s) {
+        string t = "";
+        for(int i = s.length() - 1;i >= 0;i--) 
+            t += s[i];
+        
+        return lcs(s, t, s.length());
+    }
+
+int minInsertion(string &str)
+{
+    // Write your code here.
+    return str.size() - longestPalindromeSubseq(str);
+}
