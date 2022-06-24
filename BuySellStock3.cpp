3938d67c74e1a1702ebccb0a3cc729d3ebfbc57b
Prince Dhingra <princedhingra25@gmail.com>
2022-06-23 20:38:37 -0400

    Update BuySellStock3.cpp

--- BuySellStock3.cpp
+++ BuySellStock3.cpp
@@ -21,3 +21,25 @@ int maxProfit(vector<int>& p, int n)
     vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(3, -1)));
     return f(0, n, 1, 2, p, dp);
 }
+
+
+
+int maxProfit(vector<int>& p, int n)
+{
+    // Write your code here.
+    vector<vector<vector<int>>> dp(n + 1, vector<vector<int>>(2, vector<int>(3, 0)));
+    
+    for(int i = n - 1;i >= 0;i--) {
+        for(int buy = 0;buy <= 1;buy++) {
+            for(int cap = 1;cap <= 2;cap++) {
+                if(buy == 1) {
+                    dp[i][buy][cap] =  max(-p[i] + dp[i + 1][0][cap], 0 + dp[i + 1][1][cap]);
+                } else {
+                    dp[i][buy][cap] = max(p[i] + dp[i + 1][1][cap - 1], 0 + dp[i + 1][0][cap]);
+                }
+            }
+        }
+    }
+    
+    return dp[0][1][2];
+}
