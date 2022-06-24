f828d440493f8321585615f24efab1fa34134ada
Prince Dhingra <princedhingra25@gmail.com>
2022-06-24 00:49:44 -0400

    Update longestIncreasingSubsequence.cpp

--- longestIncreasingSubsequence.cpp
+++ longestIncreasingSubsequence.cpp
@@ -21,3 +21,32 @@ int longestIncreasingSubsequence(int arr[], int n)
     vector<vector<int>> dp(n, vector<int>(n + 1, -1));
     return f(0, -1, n, arr, dp);
 }
+
+
+
+
+
+//using binary search TC O(n) and SC O(n)
+
+#include<bits/stdc++.h>
+int longestIncreasingSubsequence(int arr[], int n)
+{
+    // Write Your Code here
+    vector<int> temp;
+    temp.push_back(arr[0]);
+    int len = 1;
+    
+    for(int i = 1;i < n;i++) {
+        if(arr[i] > temp.back()) {
+            temp.push_back(arr[i]);
+            len++;
+        }
+        else {
+            int ind = lower_bound(temp.begin(), temp.end(), arr[i]) - temp.begin();
+            temp[ind] = arr[i];
+        }
+    }
+    
+    return len;
+}
+
